98b41864e3f5d986c79783a76412f3edb5fa601b
itsalessandroricci <alessandro.ricci.rcc@gmail.com>
2023-11-15 10:55:41 +0100

    First push

---
+++ NatureScan/NatureScan/NatureScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatureScanApp.swift
+//  NatureScan
+//
+//  Created by Alessandro Ricci on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NatureScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
