68566dfe5bc87ad867edb44ddfa68a9f6fced105
itsalessandroricci <alessandro.ricci.rcc@gmail.com>
2023-11-23 12:48:29 +0100

    FInal commit

---
+++ NatureScan/NatureScan/Views/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  NatureScan
+//
+//  Created by Alessandro Ricci on 21/11/23.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
---
+++ NatureScan/NatureScan/Views/AllSpeciesView.swift
@@ -0,0 +1,18 @@
+//
+//  AllSpeciesView.swift
+//  NatureScan
+//
+//  Created by Alessandro Ricci on 21/11/23.
+//
+
+import SwiftUI
+
+struct AllSpeciesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllSpeciesView()
+}
